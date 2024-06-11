74ed4c89b1334610495b178cfbfad604c88404e6
Vara Prasad Velpula <varaprasadvelpula7299@gmail.com>
2024-06-11 23:46:07 +0530

    added some icons

--- src/sections/IntroPage/IntroPage.jsx
+++ src/sections/IntroPage/IntroPage.jsx
@@ -2,6 +2,8 @@ import styles from "./IntroPage.module.css";
 import ProfileImg from "../../assets/profilepic.png";
 import colorModeImg from "../../assets/sun.svg";
 import igImg from "../../assets/ig.svg";
+import GithubIcon from "../../assets/githubIcon.svg";
+import LinkedInIcon from "../../assets/LinkedInIcon.svg";
 const IntroPage = () => {
   return (
     <section className={styles.container}>
@@ -18,9 +20,20 @@ const IntroPage = () => {
           Vara Prasad <br /> Velpula
         </h1>
         <h2>Computer Science Student</h2>
-        <a href="https://www.instagram.com/meliodas.7299/" target="_blank">
-          <img src={igImg} alt="Instagram Account" />
-        </a>
+        <span>
+          <a href="https://www.instagram.com/meliodas.7299/" target="_blank">
+            <img src={igImg} alt="Instagram Account" />
+          </a>
+          <a href="https://github.com/varaprasad7299" target="_blank">
+            <img src={GithubIcon} alt="GitHub Account" />
+          </a>
+          <a
+            href="https://in.linkedin.com/in/vara-prasad-velpula-96555a246"
+            target="_blank"
+          >
+            <img src={LinkedInIcon} alt="LinkedIn Account" />
+          </a>
+        </span>
       </div>
     </section>
   );
